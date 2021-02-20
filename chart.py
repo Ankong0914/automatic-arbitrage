5bf25823d459cc311b4d6985aa5cc1bf82c2ce94
Ankong0914 <f.kobayashi.acc@gmail.com>
2021-02-21 03:27:32 +0900

    feat: get chart

---
+++ chart.py
@@ -0,0 +1,32 @@
+import asyncio
+import csv
+import time
+
+from exchange import BitFlyer
+from exchange import GmoCoin
+
+
+@asyncio.coroutine
+def func1(exc, w, i):
+    while True:
+        exc.update_ticker()
+        data = [i, exc.ask, exc.bid, exc.timestamp]
+        w.writerow(data)
+        i += 1
+        yield from asyncio.sleep(1)
+
+
+if __name__ == "__main__":
+    bf = BitFlyer()
+    gc = GmoCoin()
+
+    with open("data/chart_bitflyer.csv", "a") as f_bf, open("data/chart_gmocoin.csv", "a") as f_gc:
+        w_bf = csv.writer(f_bf, delimiter=",")
+        w_bf.writerow(["index", "ask", "bid", "timestamp"])
+        w_gc = csv.writer(f_gc, delimiter=",")
+        w_gc.writerow(["index", "ask", "bid", "timestamp"])
+        i = 0
+
+        loop = asyncio.get_event_loop()
+        tasks = asyncio.wait([func1(bf, w_bf, i), func1(gc, w_gc, i)])
+        loop.run_until_complete(tasks)
