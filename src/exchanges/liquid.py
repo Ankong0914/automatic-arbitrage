d7f1a9562d4c2ae0f81bf5af1044d7a12508dca1
Ankong0914 <f.kobayashi.acc@gmail.com>
2021-04-07 01:39:27 +0900

    feat: get balance with liquid api

--- src/exchanges/liquid.py
+++ src/exchanges/liquid.py
@@ -2,8 +2,7 @@ import time
 from datetime import datetime
 import requests
 import json
-import hmac
-import hashlib
+import jwt
 
 from exchanges.exchange import Exchange
 
@@ -14,16 +13,16 @@ class Liquid(Exchange):
         self.NAME = "Liquid"
         self.URL = "https://api.liquid.com"
         self.TICKER_EP = "/products/5"
-        self.BALANCE_EP = ""
+        self.BALANCE_EP = "/accounts/balance"
         self.ORDER_EP = ""
         self.MIN_TRANS_UNIT = 0.001
         self.REMITTANCE_CHARGE_RATE = 0
         self.TRANS_CHARGE_RATE = 0
 
-        # with open("exchanges/key_config.json", "r") as f:
-        #     key_conf = json.load(f)
-        # self.api_key = key_conf[self.NAME]["api_key"]
-        # self.api_secret = key_conf[self.NAME]["api_secret"]
+        with open("exchanges/key_config.json", "r") as f:
+            key_conf = json.load(f)
+        self.api_key = key_conf[self.NAME]["api_key"]
+        self.api_secret = key_conf[self.NAME]["api_secret"]
 
     def update_ticker(self):
         request_url = f'{self.URL}{self.TICKER_EP}'
@@ -35,47 +34,44 @@ class Liquid(Exchange):
         self.spread = self.ask - self.bid
         self.timestamp = ticker["timestamp"]
 
-    def make_headers(self, method, path, reqBody=None):
-        timestamp = '{0}000'.format(int(time.mktime(datetime.now().timetuple())))
-        if reqBody is not None:
-            reqBody = json.dumps(reqBody)
-        else:
-            reqBody = ''
-        text = timestamp + method + path + reqBody
-        sign = hmac.new(
-            bytes(self.api_secret.encode('ascii')),
-            bytes(text.encode('ascii')),
-            hashlib.sha256
-            ).hexdigest()
+    def make_headers(self, path):
+        timestamp = datetime.now().timestamp()
+        payload = {
+            "path": path,
+            "nonce": timestamp,
+            "token_id": self.api_key
+        }
+        signature = jwt.encode(payload, self.api_secret, algorithm='HS256')
+
         headers = {
-            'API-KEY': self.api_key,
-            'API-TIMESTAMP': timestamp,
-            'API-SIGN': sign
+            'X-Quoine-API-Version': '2',
+            'X-Quoine-Auth': signature,
+            'Content-Type' : 'application/json'
         }
         return headers
 
     def update_balance(self):
-        request_url = f'{self.URL}/private{self.BALANCE_EP}'
-        headers = self.make_headers("GET", self.BALANCE_EP)
+        request_url = f'{self.URL}{self.BALANCE_EP}'
+        headers = self.make_headers(self.BALANCE_EP)
         response = requests.get(request_url, headers=headers)
-        balance = response.json()["data"]
-        
-        for currency_data in balance:
-            if currency_data["symbol"] == "JPY":
-                self.balance_jpy = int(currency_data["amount"])
-            elif currency_data["symbol"] == "BTC":
-                self.balance_btc = float(currency_data["amount"])
+        balance = response.json()
 
-    def post_order(self, side, size):
-        request_url = f'{self.URL}/private{self.ORDER_EP}'
-        reqBody = {
-            "symbol": "BTC",
-            "side": side,
-            "executionType": "MARKET",
-            "size": size
-        }
-        headers = self.make_headers("POST", self.ORDER_EP, reqBody)
-        response = requests.post(request_url, headers=headers, data=json.dumps(reqBody))
-        print(json.dumps(response.json(), indent=2))
+        for currency_data in balance:
+            if currency_data["currency"] == "JPY":
+                self.balance_jpy = int(float(currency_data["balance"]))
+            elif currency_data["currency"] == "BTC":
+                self.balance_btc = float(currency_data["balance"])
+
+    # def post_order(self, side, size):
+    #     request_url = f'{self.URL}/private{self.ORDER_EP}'
+    #     reqBody = {
+    #         "symbol": "BTC",
+    #         "side": side,
+    #         "executionType": "MARKET",
+    #         "size": size
+    #     }
+    #     headers = self.make_headers("POST", self.ORDER_EP, reqBody)
+    #     response = requests.post(request_url, headers=headers, data=json.dumps(reqBody))
+    #     print(json.dumps(response.json(), indent=2))
 
 
