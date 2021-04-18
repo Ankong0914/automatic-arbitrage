eb13a0785a0a0452992afd05dfb7e7f4f8f9ae43
Ankong0914 <f.kobayashi.acc@gmail.com>
2021-04-19 00:55:19 +0900

    feat: change algorithm

--- src/arbitrage.py
+++ src/arbitrage.py
@@ -11,8 +11,9 @@ class Arbitrage():
         self.exc2 = exc2
         self.high_ask_exc = None
         self.low_ask_exc = None
-        self.LOOP_DURATION = 1.0
-        self.trans_size_table = pd.read_csv("../data/transaction_size.csv")
+        self.LOOP_DURATION = 1.0 # TODO
+        self.MIN_TRANS_UNIT = min([self.exc1.MIN_TRANS_UNIT, self.exc2.MIN_TRANS_UNIT])
+        self.logger = logging.getLogger(__name__)
 
     async def update_tickers(self, exc1, exc2):  # TODO: more generalize
         loop = asyncio.get_event_loop()
@@ -24,26 +25,6 @@ class Arbitrage():
     def send_async_requests(self, func, args):
         loop = asyncio.get_event_loop()
         loop.run_until_complete(func(*args))
-
-    def get_status(self):  # TODO: rename
-        RATIO_THRES = 0.3
-        RATIO_THRES_HEAVY = 0.5
-
-        total_btc = self.exc1.balance_btc + self.exc2.balance_btc
-        diff_btc = abs(self.exc1.balance_btc - self.exc2.balance_btc)
-
-        if total_btc == 0:
-            status = "balanced"
-            return status
-
-        ratio = diff_btc / total_btc
-        if ratio < RATIO_THRES:
-            status = "balanced"
-        elif ratio < RATIO_THRES_HEAVY:
-            status = "unbalanced"
-        else:
-            status = "unbalanced_heavy"
-        return status
 
     def get_relation(self):
         if self.exc1.ask > self.exc2.ask:
@@ -67,54 +48,39 @@ class Arbitrage():
         margin = selling_price - buying_price - selling_charge - buying_charge
         return margin
 
-    def get_margin_expectation(self, margin):
-        MARGIN_THRES = [1000, 5000, 10000, 15000]
+    def get_margin_type(self, margin):
+        MARGIN_THRES_RATE = [0.0002, 0.0007, 0.0012, 0.0018] # 2100, 4900, 8400, 12600 
 
-        if margin < MARGIN_THRES[0]:
-            expectation = "little"
-        elif margin < MARGIN_THRES[1]:
-            expectation = "low"
-        elif margin < MARGIN_THRES[2]:
-            expectation = "mid"
-        elif margin < MARGIN_THRES[3]:
-            expectation = "high"
+        if margin < MARGIN_THRES_RATE[0] * self.high_ask_exc.ask:
+            margin_type = "little"
+        elif margin < MARGIN_THRES_RATE[1] * self.high_ask_exc.ask:
+            margin_type = "small"
+        elif margin < MARGIN_THRES_RATE[2] * self.high_ask_exc.ask:
+            margin_type = "mid"
+        elif margin < MARGIN_THRES_RATE[3] * self.high_ask_exc.ask:
+            margin_type = "large"
         else:
-            expectation = "ultra"
-        return expectation
+            margin_type = "huge"
+        return margin_type
 
-    def get_approach(self):
-        if self.high_ask_exc.balance_btc > self.low_ask_exc.balance_btc:
-            approach = "level"
-        else:
-            approach = "tilt"
-        return approach
+    def get_size(self, margin_type):
+        target_btc_ratio = {}
+        target_btc_ratio["small"] = 4/8
+        target_btc_ratio["mid"] = 5/8
+        target_btc_ratio["large"] = 7/8
+        target_btc_ratio["huge"] = 8/8
 
-    def get_size(self, expectation, status, approach):
-        trans_size = self.trans_size_table[
-                        (self.trans_size_table["expectation"] == expectation) &
-                        (self.trans_size_table["credit_status"] == status) &
-                        (self.trans_size_table["approach"] == approach)
-                    ]["credit_size"].values[0]
-        return trans_size
-    
-    def show_trans_result(self):
-        print("-------------------------------")
-        profit = self.exc1.balance_jpy + self.exc2.balance_jpy - 500000
-        print(f"profit :{profit}")
-        print("-------------------------------")
+        total_btc = self.exc1.balance_btc + self.exc2.balance_btc
+        size = total_btc * target_btc_ratio[margin_type] - self.low_ask_exc.balance_btc
+        if size < self.MIN_TRANS_UNIT:
+            size = 0
+        return size
 
     def wait_until_next_loop(self, start):
         end = time.time()
         time.sleep(max(0, self.LOOP_DURATION - (end - start)))
 
     def run(self):
-        # get balance TODO: asyncronize
-        self.exc1.update_balance()
-        self.exc2.update_balance()
-
-        # update status
-        status = self.get_status()
-
         while True:
             start = time.time()
 
@@ -127,81 +93,77 @@ class Arbitrage():
                 continue
 
             margin = self.calc_margin()
-            expectation = self.get_margin_expectation(margin)  # little ~ ultra
-            if expectation == "little":
+            margin_type = self.get_margin_type(margin)  # little ~ huge
+            if margin_type == "little":
                 self.wait_until_next_loop(start)
                 continue
 
-            approach = self.get_approach()  # level/tilt
-            trans_size = self.get_size(expectation, status, approach)  # 0 ~ 13
-            if trans_size == 0:
+            size = self.get_size(margin_type) 
+            if size == 0:
                 self.wait_until_next_loop(start)
                 continue
 
             # order TODO: asyncronize
-            # self.high_ask_exc.post_order("SELL", trans_size)
-            # self.low_ask_exc.post_order("BUY", trans_size)
+            # self.high_ask_exc.post_order("SELL", size)
+            # self.low_ask_exc.post_order("BUY", size)
 
             # get balance TODO: asyncronize
-            self.exc1.update_balance()
+            self.exc1.update_balance() # need not do this here
             self.exc2.update_balance()
 
-            # update status
-            status = self.get_status()
-            self.show_trans_result()
             self.wait_until_next_loop(start)
     
 
 class ArbitrageSimulator(Arbitrage):
     def __init__(self, exc1, exc2):
         super(ArbitrageSimulator, self).__init__(exc1, exc2)
-        self.exc1.balance_yen = 250000
+        self.exc1.balance_jpy = 250000
         self.exc1.balance_btc = 0.02
-        self.exc2.balance_yen = 250000
+        self.exc2.balance_jpy = 250000
         self.exc2.balance_btc = 0.02
+        self.chart = {}
+        self.chart[self.exc1.NAME] = pd.read_csv(f"../data/chart_{self.exc1.NAME}.csv")
+        self.chart[self.exc2.NAME] = pd.read_csv(f"../data/chart_{self.exc2.NAME}.csv")
     
-    def simulated_order(self, trans_size):
-        self.high_ask_exc.balance_jpy += self.high_ask_exc.bid * trans_size
-        self.high_ask_exc.balance_btc -= trans_size
-        self.low_ask_exc.balance_jpy -= self.low_ask_exc.ask * trans_size
-        self.low_ask_exc.balance_btc += trans_size
+    def simulated_order(self, size):
+        self.high_ask_exc.balance_jpy += self.high_ask_exc.bid * size
+        self.high_ask_exc.balance_btc -= size
+        self.low_ask_exc.balance_jpy -= self.low_ask_exc.ask * size
+        self.low_ask_exc.balance_btc += size
+
+    def update_tickers_from_charts(self, tickers):
+        self.exc1.ask = tickers[0].ask
+        self.exc1.bid = tickers[0].bid
+        self.exc1.timestamp = tickers[0].timestamp
+
+        self.exc2.ask = tickers[1].ask
+        self.exc2.bid = tickers[1].bid
+        self.exc2.timestamp = tickers[1].timestamp
 
     def simulate(self):
-        # update status
-        status = self.get_status()
-
-        while True:
-            start = time.time()
-
-            # update tickers
-            self.send_async_requests(self.update_tickers, (self.exc1, self.exc2))
+        charts_iter = zip(self.chart[self.exc1.NAME].itertuples(), self.chart[self.exc2.NAME].itertuples())
+        for ticker1, ticker2 in charts_iter:
+            # update ticker
+            self.update_tickers_from_charts([ticker1, ticker2])
 
             relation = self.get_relation()  # gap/intersection/inclusion
             if relation != "gap":
-                print(relation)
-                self.wait_until_next_loop(start)
                 continue
 
             margin = self.calc_margin()
-            expectation = self.get_margin_expectation(margin)  # little ~ ultra
-            if expectation == "little":
-                print(expectation)
-                self.wait_until_next_loop(start)
+            print(margin)
+            margin_type = self.get_margin_type(margin)  # little ~ ultra
+            if margin_type == "little":
                 continue
 
-            approach = self.get_approach()  # level/tilt
-            trans_size = self.get_size(expectation, status, approach)  # 0 ~ 13
-            if trans_size == 0:
-                print(trans_size)
-                self.wait_until_next_loop(start)
+            size = self.get_size(margin_type)  # 0 ~ 13
+            if size == 0:
                 continue
-
-            self.simulated_order(trans_size)
-
-            # update status
-            status = self.get_status()
-
-            self.wait_until_next_loop(start)
+            
+            self.simulated_order(size)
+            profit = int(self.exc1.balance_jpy + self.exc2.balance_jpy - 500000)
+            print(profit, margin_type, size, self.exc1.balance_btc, self.exc2.balance_btc)
+    
 
 
 if __name__ == "__main__":
