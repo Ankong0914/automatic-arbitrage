c518a9539ff02695735aa4b782e4051a2870a0d3
Ankong0914 <f.kobayashi.acc@gmail.com>
2021-04-23 01:24:13 +0900

    feat: continuous fetch tickers

--- src/chart.py
+++ src/chart.py
@@ -1,32 +1,46 @@
 import asyncio
 import csv
 import time
+import requests
 
-from exchanges.bit_flyer import BitFlyer
-from exchanges.gmo_coin import GmoCoin
+from exchanges.coin_check import CoinCheck
+from exchanges.liquid import Liquid
 
 
-@asyncio.coroutine
-def func1(exc, w, i):
-    while True:
-        exc.update_ticker()
-        data = [i, exc.ask, exc.bid, exc.timestamp]
-        w.writerow(data)
-        i += 1
-        yield from asyncio.sleep(1)
+async def update_tickers(exc1, exc2):  # TODO: more generalize
+    loop = asyncio.get_event_loop()
+    future1 = loop.run_in_executor(None, exc1.update_ticker)
+    future2 = loop.run_in_executor(None, exc2.update_ticker)
+    await future1
+    await future2
+
+def send_async_requests(func, args):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(func(*args))
 
 
 if __name__ == "__main__":
-    bf = BitFlyer()
-    gc = GmoCoin()
+    cc = CoinCheck()
+    liq = Liquid()
+    
+    i = 0
 
-    with open("data/chart_bitflyer2.csv", "a") as f_bf, open("data/chart_gmocoin2.csv", "a") as f_gc:
-        w_bf = csv.writer(f_bf, delimiter=",")
-        w_bf.writerow(["index", "ask", "bid", "timestamp"])
-        w_gc = csv.writer(f_gc, delimiter=",")
-        w_gc.writerow(["index", "ask", "bid", "timestamp"])
-        i = 0
-
-        loop = asyncio.get_event_loop()
-        tasks = asyncio.wait([func1(bf, w_bf, i), func1(gc, w_gc, i)])
-        loop.run_until_complete(tasks)
+    with open("../data/chart_coincheck_test.csv", "a") as f_cc, open("../data/chart_liquid_test.csv", "a") as f_liq:
+        w_cc = csv.writer(f_cc, delimiter=",")
+        w_liq = csv.writer(f_liq, delimiter=",")
+        w_cc.writerow(["index", "ask", "bid", "timestamp"])
+        w_liq.writerow(["index", "ask", "bid", "timestamp"])
+        
+        while True:
+            start = time.time()
+            i += 1
+            print(i)
+            try:
+                send_async_requests(update_tickers, (cc, liq))
+            except requests.exceptions.RequestException as e:
+                time.sleep(max(0, 1.1 - (end - start)))
+                continue
+            w_cc.writerow([i, cc.ask, cc.bid, cc.timestamp])
+            w_liq.writerow([i, liq.ask, liq.bid, liq.timestamp])
+            end = time.time()
+            time.sleep(max(0, 1.1 - (end - start)))
